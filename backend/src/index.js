d21e05f4debcd25eea8c938e8f4aee81e5718bce
RBryan05 <bryanrauda05@gmail.com>
2026-04-28 17:43:38 -0600

    feat: actualizar Dockerfile y corregir puerto en index.js; renombrar título en index.html

--- backend/src/index.js
+++ backend/src/index.js
@@ -9,7 +9,7 @@ const productosRoutes = require('./routes/productos');
 const movimientosRoutes = require('./routes/movimientos');
 
 const app = express();
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 
 connectDB();
 
