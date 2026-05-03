af07e51f1c2cddcad4a29cecb29a45ace9fb08e7
LeonidasVS <villafrancosibrian@gmail.com>
2026-05-02 23:02:20 -0600

    feat: Implementar endpoint de login con JWT.

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -1,7 +1,12 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { registrarUsuario } = require('../controllers/authController');
+const {
+  registrarUsuario,
+  loginUsuario,
+} = require("../controllers/authController");
 
-router.post('/registro', registrarUsuario);
+router.post("/registro", registrarUsuario);
+
+router.post("/login", loginUsuario);
 
 module.exports = router;
